b3612144bedcc0528146e76cb79d1354fb6ca289
Yuriy_Tkach <Yuriy_Tkach@EPUAKYIW6057.kyiv.epam.com>
2016-03-11 15:20:13 +0200

    Added domain classes and service interfaces

---
+++ src/main/java/ua/epam/spring/hometask/service/AuditoriumService.java
@@ -0,0 +1,31 @@
+package ua.epam.spring.hometask.service;
+
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import ua.epam.spring.hometask.domain.Auditorium;
+
+/**
+ * @author Yuriy_Tkach
+ */
+public interface AuditoriumService {
+
+    /**
+     * Getting all auditoriums from the system
+     * 
+     * @return set of all auditoriums
+     */
+    public @Nonnull Set<Auditorium> getAll();
+
+    /**
+     * Finding auditorium by name
+     * 
+     * @param name
+     *            Name of the auditorium
+     * @return found auditorium or <code>null</code>
+     */
+    public @Nullable Auditorium getByName(@Nonnull String name);
+
+}
